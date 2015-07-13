93414c5c1f1a7128844bffd20b8d59b696d8949d
Chris Kellendonk <ckellendonk@konradgroup.com>
2015-07-13 14:49:28 -0400

    Fix race condition

--- vanilla/client_app/js/main.js
+++ vanilla/client_app/js/main.js
@@ -4,17 +4,16 @@ var $mainContainer = $("#main");
 
 var templates = ['main', 'question'];
 
-// TODO: Return promise indicating all templates loaded
 function registerTemplates(templates) {
-  templates.forEach(function(templateName) {
-    tload('templates/' + templateName + '.handlebars').done(function(template) {
+  return $.when.apply($, templates.map(function(templateName) {
+    return tload('templates/' + templateName + '.handlebars').done(function(template) {
       Handlebars.registerPartial(templateName, template);
-    });
+    }));
   })
 }
 
-registerTemplates(templates);
-
-tload('templates/main.handlebars').done(function(template) {
-  $mainContainer.html(template());
+registerTemplates(templates).done(function() {
+  tload('templates/main.handlebars').done(function(template) {
+    $mainContainer.html(template());
+  });
 });
